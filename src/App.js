429b89e961c7c65da4a7675f3448b192fed47476
Rudolf Pailer <rudolf.pailer@at.ibm.com>
2021-05-14 12:40:35 +0200

    long press button + release button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import useLongPress from "./useLongPress";
 
 function App() {
+  const [longPressCount, setlongPressCount] = useState(0)
+  const [clickCount, setClickCount] = useState(0)
+
+  const onLongPress = () => {
+    console.log('longpress is triggered');
+    setlongPressCount(longPressCount + 1);
+  };
+
+  const onRelease = () => {
+    console.log('longpress is released');
+    
+  };
+
+  const onClick = () => {
+    console.log('click is triggered')
+    setClickCount(clickCount + 1);
+  }
+
+  const defaultOptions = {
+    shouldPreventDefault: true,
+    delay: 500,
+  };
+  const longPressEvent = useLongPress(onLongPress, onRelease, onClick, defaultOptions);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button {...longPressEvent}>use  Loooong  Press</button>
+      <span>Long press count: {longPressCount}</span>
+      <span>Click count: {clickCount}</span>
     </div>
+
   );
 }
 
