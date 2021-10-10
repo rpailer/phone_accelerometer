79c134dc64ddbd8dbfb067740cb866f4ae5e5bf6
Rudolf Pailer <rudolf.pailer@at.ibm.com>
2021-10-10 18:07:24 +0200

    iphone permission

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -52,17 +52,21 @@ export default function Score () {
                     z: event.acceleration.z
                 },
             };
+            console.log("before");
             if (dataObj.dataArray.at(-1)) {
                 console.log("last " + dataObj.dataArray.at(-1).timestamp);
                 let timeDiff = now - dataObj.dataArray.at(-1).timestamp;
                 if (timeDiff > delay) {
                     console.log("set additional accel data " + data.date);
                     setDataObj({ dataArray: [...dataObj.dataArray, data]});
-                }
+                } else {
+                    console.log("ignore");
+                }  
             } else {
                 console.log("set new accel data " + data.date);
                 setDataObj({ dataArray: [...dataObj.dataArray, data]});
             }
+            console.log("afer");
         }
     }
 
