66e60637115042589403ec9a87d582317fcb96e5
Rudolf Pailer <rudolf.pailer@at.ibm.com>
2021-10-10 12:51:39 +0200

    iphone permission

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -138,7 +138,7 @@ export default function Score () {
                     };
         // eslint-disable-next-line
     }
-          }, [dataObj]);
+          }, [recording, dataObj]);
 
     return (
         <div>
